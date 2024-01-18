77195f4be252effc589c693aaff772d57a5eeaa4
Keestle <yeeern98@gmail.com>
2024-01-18 15:07:07 +0800

    Changes include : - The use of template for product cards, - Fetching API from fakestore API

---
+++ Other Package Template.js
@@ -0,0 +1,39 @@
+// Sample product data
+const products = [];
+
+
+// Create a function which call an external API to get a collection of data (Excercise 9-10)
+function fetchProductdata(){
+fetch('https://fakestoreapi.com/products')
+    .then((response) => {if (!response.ok) {
+        throw new Error('Network response was not okay');
+    } 
+    return response.json();
+ })
+    .then((json) => {
+        products.push(...json)
+    })
+    .catch((error)=> {
+    console.error("Error fetching data:", error)
+    });
+}
+
+// Function to populate the template with product data and append to the container
+function addCards() {
+    const template = document.getElementById('product-cardTemplate');
+    const container = document.getElementById('productContainer');
+
+    products.forEach(product => {
+        // Clone the template content
+        const clone = document.importNode(template.content, true);
+
+        // Update the cloned elements with product data
+        clone.querySelector('.productName').innerText = product.name;
+        clone.querySelector('#productPrice').innerText = product.price;
+        clone.querySelector('.productDescription').innerText = product.description;
+
+        // Append the cloned template to the container
+        container.appendChild(clone);
+    });
+}
+fetchProductdata()
